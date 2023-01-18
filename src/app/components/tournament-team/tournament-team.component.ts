f0e5d855a6662fa47cad2693b77da71fa634e896
Niccolò Biondi <niccolo@3logic.it>
2023-01-18 15:20:59 +0100

    fix avatars

--- src/app/components/tournament-team/tournament-team.component.ts
+++ src/app/components/tournament-team/tournament-team.component.ts
@@ -1,28 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TournamentService } from 'src/app/services/tournament.service';
 
-const IMAGE_PATH = "assets/images/";
+const IMAGE_PATH = "assets/avatars/";
 
 @Component({
   selector: 'tournament-team',
   templateUrl: './tournament-team.component.html',
   styleUrls: ['./tournament-team.component.scss']
 })
-export class TournamentTeamComponent {
+export class TournamentTeamComponent implements OnInit {
   @Input() player1: string = '';
   @Input() player2: string = '';
 
+  public image1: string = '';
+  public image2: string = '';
+  private imageDefault = IMAGE_PATH + 'default.jpg';
+
+  playerWithAvatars = ['adriana', 'andrea', 'bene', 'carlo', 'claudio', 'david', 'default', 'giulia', 'leonardo', 'marco', 'mario', 'michelangelo', 'paola', 'peco']
   constructor(
     private tournamentService: TournamentService
   ) { }
+
+  ngOnInit(): void {
+    this.image1 = this.getImage(this.player1);
+    this.image2 = this.getImage(this.player2);
+  }
 
   getTeamName(): string {
     return this.tournamentService.getTeamName(this.player1, this.player2);
   }
 
   getImage(playerName: string): string {
-    return IMAGE_PATH + 'default.jpg';
-    return IMAGE_PATH + playerName + '.jpg';
+    if (this.playerWithAvatars.includes(playerName.toLowerCase())) {
+      return IMAGE_PATH + playerName.toLowerCase() + '.jpg';
+    }
+    return this.imageDefault;
   }
 
 }
